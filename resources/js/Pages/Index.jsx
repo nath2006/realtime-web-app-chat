4fb23059cdaa972a44a4d93fe9141b2ad9718db4
Nath2006 <ananthapindika739@gmail.com>
2025-11-23 23:58:01 +0700

    feat: Refactor AuthenticatedLayout and create Index page for WebChat interface

---
+++ resources/js/Pages/Index.jsx
@@ -0,0 +1,32 @@
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+import { Head} from '@inertiajs/react';
+import { Settings } from 'lucide';
+
+export default function Welcome({ auth }) {
+    return (
+        <AuthenticatedLayout>
+            <Head title="WebChat" />
+            <div className="flex">
+                <div className="w-2/6 bg-gray-900 border-r border-gray-800">
+                    <div className="pt-4 flex flex-col h-screen">
+                        <div className="px-4">
+                            <div className='flex justify-between mb-2'>
+                                <h1 className='text-white text-xl font-bold'>WebChat</h1>
+                                <div>
+                                    # Settings #
+                                </div>
+                            </div>
+                                # SEARCH #
+                        </div>
+                        <div className="flex-1 px-4 overflow-y-auto">
+                            # ROOM #
+                        </div>
+                    </div>
+                </div>
+                <div className="relative w-4/6">
+                    # CHAT #
+                </div>
+            </div>
+        </AuthenticatedLayout>
+    );
+}
